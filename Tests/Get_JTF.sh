68527b6c589e65b953b213831e1dfcafd5a53cf6
Chris Watson <chris.watson@7digital.com>
2013-01-29 10:24:33 +0000

    Update get framework script

--- Tests/Get_JTF.sh
+++ Tests/Get_JTF.sh
@@ -1,6 +1,5 @@
-git clone --depth 1 --branch master git://github.com/c24w/JavaScript-Testing-Framework.git JTF;
+git clone --depth 1 --branch master --recursive git://github.com/c24w/JavaScript-Testing-Framework.git JTF;
 cd ./JTF;
-git submodule init;
-git submodule update;
 ls -a | grep -v 'src' | xargs rm -rf;
 find -type f -name '.*' -o -name '*.md' | xargs rm -f
+cd ..
